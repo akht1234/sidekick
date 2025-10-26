cf619879f2b7583de672a10152bc313e6add4aad
Akshat Manas <138560286+akht1234@users.noreply.github.com>
2025-10-26 20:05:53 +0530

    Refactor GoogleSerperAPIWrapper initialization

--- sidekick_tools.py
+++ sidekick_tools.py
@@ -10,13 +10,16 @@ from langchain_experimental.tools import PythonREPLTool
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 
-
-
+# Load .env variables
 load_dotenv(override=True)
+
 pushover_token = os.getenv("PUSHOVER_TOKEN")
 pushover_user = os.getenv("PUSHOVER_USER")
 pushover_url = "https://api.pushover.net/1/messages.json"
-serper = GoogleSerperAPIWrapper()
+
+# Pass SERPER_API_KEY explicitly
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+serper = GoogleSerperAPIWrapper(serper_api_key=SERPER_API_KEY)
 
 async def playwright_tools():
     playwright = await async_playwright().start()
@@ -24,23 +27,24 @@ async def playwright_tools():
     toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=browser)
     return toolkit.get_tools(), browser, playwright
 
-
 def push(text: str):
     """Send a push notification to the user"""
-    requests.post(pushover_url, data = {"token": pushover_token, "user": pushover_user, "message": text})
+    requests.post(pushover_url, data={"token": pushover_token, "user": pushover_user, "message": text})
     return "success"
-
 
 def get_file_tools():
     toolkit = FileManagementToolkit(root_dir="sandbox")
     return toolkit.get_tools()
 
-
 async def other_tools():
-    push_tool = Tool(name="send_push_notification", func=push, description="Use this tool when you want to send a push notification")
+    push_tool = Tool(
+        name="send_push_notification", 
+        func=push, 
+        description="Use this tool when you want to send a push notification"
+    )
     file_tools = get_file_tools()
 
-    tool_search =Tool(
+    tool_search = Tool(
         name="search",
         func=serper.run,
         description="Use this tool when you want to get the results of an online web search"
@@ -51,5 +55,4 @@ async def other_tools():
 
     python_repl = PythonREPLTool()
     
-    return file_tools + [push_tool, tool_search, python_repl,  wiki_tool]
-
+    return file_tools + [push_tool, tool_search, python_repl, wiki_tool]
